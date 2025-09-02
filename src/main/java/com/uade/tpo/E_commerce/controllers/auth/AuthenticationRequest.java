e00440659c9ea71ccec625661eb5fde97e9d0698
valenrocca <136339671+valenroccaZB@users.noreply.github.com>
2025-09-02 11:49:21 -0300

    log.-in sin testear

---
+++ src/main/java/com/uade/tpo/E_commerce/controllers/auth/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.uade.tpo.E_commerce.controllers.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
--- src/main/java/com/uade/tpo/E_commerce/entity/User.java
+++ src/main/java/com/uade/tpo/E_commerce/entity/User.java
@@ -1,20 +1,31 @@
 package com.uade.tpo.E_commerce.entity;
 import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-public class User {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
@@ -25,13 +36,41 @@ public class User {
     @Column
     private String mail;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @OneToMany(mappedBy="user") // se refiere a la clase user entera no solo al id
     private List<Order> orders; // un usuario puede tener muchas ordenes
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
-    @ManyToMany
-    @JoinTable(
-        name="user_role",
-        joinColumns= @JoinColumn(name="user_id"), 
-        inverseJoinColumns = @JoinColumn(name="role_id"))
-    private List<Role> roles;
+    @Override
+    public String getUsername() {
+        return mail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+   
 }
